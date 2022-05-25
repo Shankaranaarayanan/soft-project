1a5d77f4009b7c1b56b0059a31addfcff833f210
Shankaranaarayanan <shankaranaarayananthiru0906@gmail.com>
2022-05-25 21:58:08 +0530

    added objects and models

--- project/soft_project/admin.py
+++ project/soft_project/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from soft_project.models import keys,files
 # Register your models here.
+
+admin.site.register(keys)
+admin.site.register(files)
--- project/soft_project/views.py
+++ project/soft_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-
+from soft_project.models import files,keys
 # Create your views here.
 
 def home_page(request):
@@ -9,7 +9,13 @@ def home_page(request):
         path = request.POST.get('text')
         text = request.POST.get('content')
         l=find_files(path,text)
-        print(l)
+        t=keys(key=text)
+        t.save()
+        i=keys.objects.latest('id1')
+        print(i)
+        for j in l:
+            f=files(file=j,id1=i)
+            f.save()
     return render(request,template_name='soft_project/test.html')
 
 
@@ -17,15 +23,15 @@ def find_files(path,key):
     l=[]
     for i in os.listdir(path):
         if key.lower() in i.lower():
-            l.append(path+i)
+            l.append(i)
         filepath=path+'\\'+i
         if(os.path.isfile(filepath)):
             file = open(filepath)
             for j in file:
                 if key in j:
-                    l.append(filepath)
+                    l.append(i)
             file.close()
         else:
             for i in find_files(filepath,key):
-                l.append(filepath)
+                l.append(i)
     return l
